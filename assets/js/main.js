7f291a321a2facdf9375890dd88f636ab1c6b6ec
Robert Marshall <cartransport2016@outlook.com>
2018-10-01 09:47:48 +0000

    fix filepath issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-
+// create locations coordinates offshore close to named town to get marine information for that area.
     var locations = [
         ["Penzance", 50.070092, -5.767671],
         ["St Ives", 50.250562, -5.491298],
@@ -9,26 +9,24 @@
     ];
 
 
-    //var lat = locations[1];
-    //var lng = locations[2];
+  //set parameters for api information we need
     const params = 'waveHeight,airTemperature';
 
 
 
 
-
+//initiate map
     function initMap() {
 
-        var map = new google.maps.Map(document.getElementById('map'), {
+        var map = new google.maps.Map(document.getElementById('map-canvas'), {
             zoom: 9,
-            center: { lat: 50.652433, lng: -4.142118 }
+            center: { lat: 50.738, lng: -4.002  }
         });
 
 
 
 
-        // Info Window Content
-
+        // Info Window initialize
         var infoWindow = new google.maps.InfoWindow(),
             marker, i;
 
@@ -37,8 +35,6 @@
         var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
         //set markers on map
         for (i = 0; i < locations.length; i++) {
-
-
             marker = new google.maps.Marker({
                 position: { lat: locations[i][1], lng: locations[i][2] },
                 map: map,
@@ -47,7 +43,7 @@
             });
 
 
-
+// gets relevant api data when offshore marker is clicked
             google.maps.event.addListener(marker, 'click', (function(marker, i) {
                 return function() {
                     fetch(`https://api.stormglass.io/point?lat=${locations[i][1]}&lng=${locations[i][2]}&params=${params}`, {
@@ -57,10 +53,16 @@
                     }).then(function(response) {
                         return response.json();
                     }).then(function(data) {
+                       
                         console.log(data);
+                        
+
+                      document.getElementById("waves").innerHTML = data;
                     });
+                    
                     infoWindow.setContent(locations[i][0]);
                     infoWindow.open(map, marker);
+                    
                 };
 
 
@@ -68,6 +70,11 @@
 
 
         }
+        
     }
+    
+    
 
     initMap();
+    
+    
