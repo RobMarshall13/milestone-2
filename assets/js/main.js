036cf1bc2d1a1f6cdb4553043a0f09513d3f49b0
Robert Marshall <cartransport2016@outlook.com>
2018-10-04 11:01:55 +0000

    data display problem

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ var locations = [
 
 
 //set parameters for api information we need
-const params = 'waveHeight,airTemperature';
+const params = 'swellHeight,swellDirection,swellPeriod,windDirection,windSpeed,waterTemperature,airTemperature,visibility';
 
 var hours;
 var waves;
@@ -45,52 +45,43 @@ function initMap() {
         // gets relevant api data when offshore marker is clicked
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
-                fetch(`https://api.stormglass.io/point?lat=${locations[i][1]}&lng=${locations[i][2]}&params=${params}`, {
+                fetch(`https://api.stormglass.io/point?lat=${locations[i][1]}&lng=${locations[i][2]}&params=${params}&source=noaa`, {
                     headers: {
-                        'Authorization': '9314edd6-c0d9-11e8-9f7a-0242ac130004-9314eee4-c0d9-11e8-9f7a-0242ac130004'
-                        // 'Authorization': '7efc5c42-c57c-11e8-9f7a-0242ac130004-7efc5d5a-c57c-11e8-9f7a-0242ac130004'
+                        //'Authorization': '9314edd6-c0d9-11e8-9f7a-0242ac130004-9314eee4-c0d9-11e8-9f7a-0242ac130004'
+                         'Authorization': '7efc5c42-c57c-11e8-9f7a-0242ac130004-7efc5d5a-c57c-11e8-9f7a-0242ac130004'
+                         //'Authorization': 'f1114c1a-c71c-11e8-83ef-0242ac130004-f1114d28-c71c-11e8-83ef-0242ac130004'
                     }
+                    
                 }).then(function(response) {
+                     
                     return response.json();
+                   
                 }).then(function(data) {
-
-
-                    // console.log(data['hours']);
-                    // console.log(data.hours);
-                    // console.log('First item in hours with bracket notation', data.hours[0]);
-                    // console.log('first item NOAA value', data['hours'][0]['airTemperature'][3]['value']);
-                    hours = data['hours'];
-                    for (i = 0; i < data['hours'].length; i += 6)
+                    htmlString = '';
+                     hours = data['hours'];
+                    for (i = 0; i < data['hours'].length; i+=12) {
                         console.log(hours[i]);
+                   const get = (p, o) =>
+                    p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : null, o);
+                  
                         
-                       document.getElementById("sHeight0").innerHTML = data['hours'][6]['waveHeight'][3]['value'];
-                        document.getElementById("sHeight1").innerHTML = data['hours'][12]['waveHeight'][3]['value'];
-                        document.getElementById("sHeight2").innerHTML = data['hours'][18]['waveHeight'][3]['value'];
-                        document.getElementById("sHeight3").innerHTML = data['hours'][24]['waveHeight'][3]['value'];
-
-
-
-                        document.getElementById("air").innerHTML = `Air Temp:` + data['hours'][6]['airTemperature'][1]['value'] + 'degrees c';
-
-                    var clickCount = 0; // variable to store how many times the button has been clicked        
-
-                    // Add an event listener to the button which will allow you to fire the every6Hours function
-                    $('#next6hours').on('click', function() {
-
-                        clickCount += 24;
-
-
-
-                        document.getElementById("sHeight0").innerHTML =  data['hours'][clickCount]['waveHeight'][3]['value'];
-                        document.getElementById("sHeight1").innerHTML = data['hours'][clickCount + 6]['waveHeight'][3]['value'];
-                        document.getElementById("sHeight2").innerHTML = data['hours'][clickCount + 12]['waveHeight'][3]['value'];
-                        document.getElementById("sHeight3").innerHTML = data['hours'][clickCount + 18]['waveHeight'][3]['value'];
-
-
-
-                        document.getElementById("air").innerHTML = `Air Temp:` + data['hours'][clickCount]['airTemperature'][1]['value'] + 'degrees c';
-
-                    });
+                        htmlString += '<tr>';
+                         //htmlString += '<td>'+hours,i.time+'</td>';
+                         htmlString += '<td>'+(get([ 'airTemperature', 0, 'value'],hours))+'</td>';
+                        //  htmlString += '<td>'+data['hours'][0]['swellHeight'][0]['value']+'</td>';
+                        //  htmlString += '<td>'+data['hours'][0]['swellPeriod'][0]['value']+'</td>';
+                        //  htmlString += '<td>'+data['hours'][0]['windDirection'][0]['value']+'</td>';
+                        //  htmlString += '<td>'+data['hours'][0]['windSpeed'][0]['value']+'</td>';
+                        //  htmlString += '<td>'+data['hours'][0]['waterTemperature'][0]['value']+'</td>';
+                        //  htmlString += '<td>'+data['hours'][0]['airTemperature'][0]['value']+'</td>';
+                        //  htmlString += '<td>'+data['hours'][0]['visibility'][0]['value']+'</td>';
+                         htmlString += '</tr>';
+                    
+                    }
+                    
+                    $('#tideTable').append(htmlString);
+                    
+                
                 });
                 infoWindow.setContent(locations[i][0]);
                 infoWindow.open(map, marker);
@@ -109,3 +100,18 @@ function initMap() {
 
 
 initMap();
+
+
+// $.each(hours, function(key,value){
+//                         hours += '<tr>';
+//                         hours += '<td>'+value.time+'</td>';
+//                         hours += '<td>'+value.swellDirection+'</td>';
+//                         hours += '<td>'+data['hours'][0]['swellHeight'][0]['value']+'</td>';
+//                         hours += '<td>'+data['hours'][0]['swellPeriod'][0]['value']+'</td>';
+//                         hours += '<td>'+data['hours'][0]['windDirection'][0]['value']+'</td>';
+//                         hours += '<td>'+data['hours'][0]['windSpeed'][0]['value']+'</td>';
+//                         hours += '<td>'+data['hours'][0]['waterTemperature'][0]['value']+'</td>';
+//                         hours += '<td>'+data['hours'][0]['airTemperature'][0]['value']+'</td>';
+//                         hours += '<td>'+data['hours'][0]['visibility'][0]['value']+'</td>';
+//                         hours += '</tr>';
+//                     });
