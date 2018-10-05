24c1f59d35234985b3a53a25e4f4d75b65f129ff
Robert Marshall <cartransport2016@outlook.com>
2018-10-05 09:01:35 +0000

    displaying data

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,8 +11,9 @@ var locations = [
 //set parameters for api information we need
 const params = 'swellHeight,swellDirection,swellPeriod,windDirection,windSpeed,waterTemperature,airTemperature,visibility';
 
+var getInfo;
 var hours;
-var waves;
+var htmlString;
 
 //initiate map
 function initMap() {
@@ -43,13 +44,18 @@ function initMap() {
 
 
         // gets relevant api data when offshore marker is clicked
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+       getInfo = google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        //   if(hours != null){
+        //         $('#tideTable').remove(htmlString);
+        //   }else{
+            
             return function() {
+                 
                 fetch(`https://api.stormglass.io/point?lat=${locations[i][1]}&lng=${locations[i][2]}&params=${params}&source=noaa`, {
                     headers: {
                         //'Authorization': '9314edd6-c0d9-11e8-9f7a-0242ac130004-9314eee4-c0d9-11e8-9f7a-0242ac130004'
-                         'Authorization': '7efc5c42-c57c-11e8-9f7a-0242ac130004-7efc5d5a-c57c-11e8-9f7a-0242ac130004'
-                         //'Authorization': 'f1114c1a-c71c-11e8-83ef-0242ac130004-f1114d28-c71c-11e8-83ef-0242ac130004'
+                         //'Authorization': '7efc5c42-c57c-11e8-9f7a-0242ac130004-7efc5d5a-c57c-11e8-9f7a-0242ac130004'
+                         'Authorization': 'f1114c1a-c71c-11e8-83ef-0242ac130004-f1114d28-c71c-11e8-83ef-0242ac130004'
                     }
                     
                 }).then(function(response) {
@@ -58,29 +64,31 @@ function initMap() {
                    
                 }).then(function(data) {
                     htmlString = '';
+                    
                      hours = data['hours'];
-                    for (i = 0; i < data['hours'].length; i+=12) {
+                    for (i = 0; i < data['hours'].length; i+=24) {
                         console.log(hours[i]);
                    const get = (p, o) =>
                     p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : null, o);
                   
-                        
+                         
                         htmlString += '<tr>';
-                         //htmlString += '<td>'+hours,i.time+'</td>';
-                         htmlString += '<td>'+(get([ 'airTemperature', 0, 'value'],hours))+'</td>';
-                        //  htmlString += '<td>'+data['hours'][0]['swellHeight'][0]['value']+'</td>';
-                        //  htmlString += '<td>'+data['hours'][0]['swellPeriod'][0]['value']+'</td>';
-                        //  htmlString += '<td>'+data['hours'][0]['windDirection'][0]['value']+'</td>';
-                        //  htmlString += '<td>'+data['hours'][0]['windSpeed'][0]['value']+'</td>';
-                        //  htmlString += '<td>'+data['hours'][0]['waterTemperature'][0]['value']+'</td>';
-                        //  htmlString += '<td>'+data['hours'][0]['airTemperature'][0]['value']+'</td>';
-                        //  htmlString += '<td>'+data['hours'][0]['visibility'][0]['value']+'</td>';
+                         htmlString += '<td>'+hours[i].time+'</td>';
+                         htmlString += '<td>'+(get([ 'swellDirection', 0, 'value'],hours[i]))+'</td>';
+                         htmlString += '<td>'+(get([ 'swellHeight', 0, 'value'],hours[i]))+'</td>';
+                         htmlString += '<td>'+(get([ 'swellPeriod', 0, 'value'],hours[i]))+'</td>';
+                         htmlString += '<td>'+(get([ 'windDirection', 0, 'value'],hours[i]))+'</td>';
+                         htmlString += '<td>'+(get([ 'windSpeed', 0, 'value'],hours[i]))+'</td>';
+                         htmlString += '<td>'+(get([ 'waterTemperature', 0, 'value'],hours[i]))+'</td>';
+                         htmlString += '<td>'+(get([ 'airTemperature', 0, 'value'],hours[i]))+'</td>';
+                         htmlString += '<td>'+(get([ 'visibility', 0, 'value'],hours[i]))+'</td>';
                          htmlString += '</tr>';
                     
                     }
-                    
+                   
                     $('#tideTable').append(htmlString);
-                    
+                  
+                   
                 
                 });
                 infoWindow.setContent(locations[i][0]);
@@ -88,8 +96,8 @@ function initMap() {
 
 
             };
-
-
+//}
+ 
         })(marker, i));
 
 
@@ -102,16 +110,3 @@ function initMap() {
 initMap();
 
 
-// $.each(hours, function(key,value){
-//                         hours += '<tr>';
-//                         hours += '<td>'+value.time+'</td>';
-//                         hours += '<td>'+value.swellDirection+'</td>';
-//                         hours += '<td>'+data['hours'][0]['swellHeight'][0]['value']+'</td>';
-//                         hours += '<td>'+data['hours'][0]['swellPeriod'][0]['value']+'</td>';
-//                         hours += '<td>'+data['hours'][0]['windDirection'][0]['value']+'</td>';
-//                         hours += '<td>'+data['hours'][0]['windSpeed'][0]['value']+'</td>';
-//                         hours += '<td>'+data['hours'][0]['waterTemperature'][0]['value']+'</td>';
-//                         hours += '<td>'+data['hours'][0]['airTemperature'][0]['value']+'</td>';
-//                         hours += '<td>'+data['hours'][0]['visibility'][0]['value']+'</td>';
-//                         hours += '</tr>';
-//                     });
